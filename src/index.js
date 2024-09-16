ddbc85ab249bb6ecec7f40483569fe66ea3945f4
karthikeyan <karthikeyan.muthukrishnan@havells.com>
2024-09-16 19:43:44 +0530

    setting-up-server

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const serverConfig=require('./config/serverConfig')
 const setupServer = async ()=>{
     const app=express();
     const PORT=serverConfig.PORT
+    
+    app.use(express.json())
     app.listen(PORT,()=>{
         console.log(`Server up at port ${serverConfig.PORT}`)
     })
